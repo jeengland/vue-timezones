438380a7411600bc7b5b52656f0fe6bbea7c83a1
unknown <jacob.e.england@gmail.com>
2021-01-27 15:01:07 -0500

    Added initial files for client

---
+++ vue-timezones/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id='app'>
+    <time-zone></time-zone>
+  </div>
+</template>
+
+<script>
+import TimeZone from './components/TimeZone.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TimeZone
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-timezones/src/components/TimeZone.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <form @submit.prevent>
+            <input type='number' name='hours' min='1' max='12' placeholder='HH' />
+            <input type='number' name='minutes' min='0' max='59' placeholder="MM" />
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
